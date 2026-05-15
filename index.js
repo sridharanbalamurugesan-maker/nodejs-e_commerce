13b3a238bd80ed07317ed98b517c7e81be2969bf
Sridharan B <dsri1206@gmail.com>
2026-05-15 12:19:10 +0530

    Added gitignore

--- index.js
+++ index.js
@@ -1,25 +1,36 @@
-const express=require('express');
-const connect=require('./Config/config');
-const UserRouter=require('./Routes/userRoute')
-const categoryRouter=require('./Routes/categoryRoute');
-const productRoute=require('./Routes/productRoute');
-const orderRouter=require('./Routes/orderRoute');
-const paymentRouter=require('./Routes/paymentRoute');
-const cors=require('cors');
-const app=express();
+const express = require('express');
+const connect = require('./Config/config');
+const UserRouter = require('./Routes/userRoute');
+const categoryRouter = require('./Routes/categoryRoute');
+const productRoute = require('./Routes/productRoute');
+const orderRouter = require('./Routes/orderRoute');
+const paymentRouter = require('./Routes/paymentRoute');
+const cors = require('cors');
 const path = require("path");
-app.use(cors());
+require('dotenv').config(); 
+
+const app = express();
+
+app.use(cors({
+  origin: process.env.FRONTEND_URL || "*",
+  credentials: true
+}));
+
 app.use(express.json());
 
-connect();
-app.use('/category',express.static(path.join(__dirname, "category")));
-app.use('/product',express.static(path.join(__dirname, "product")));
-app.use('/api',UserRouter)
-app.use('/category',categoryRouter);
-app.use('/product',productRoute);
-app.use('/order',orderRouter);
-app.use('/payment',paymentRouter);
+app.use('/category', express.static(path.join(__dirname, "category")));
+app.use('/product', express.static(path.join(__dirname, "product")));
 
-app.listen(5000,()=>{
-    console.log('server start')
-})
+app.use('/api', UserRouter);
+app.use('/category', categoryRouter);
+app.use('/product', productRoute);
+app.use('/order', orderRouter);
+app.use('/payment', paymentRouter);
+
+connect();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
